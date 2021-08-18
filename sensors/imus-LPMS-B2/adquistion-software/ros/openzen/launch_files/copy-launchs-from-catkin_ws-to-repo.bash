d75f8a072c7334bf2df657dd2077a5e0043b4bb8
Miguel Xochicale <perez.xochicale@gmail.com>
2021-08-18 18:04:55 +0100

    adds lpmsb2-two-display-record.launch and lpmsb2-two-playback-data.launch

--- sensors/imus-LPMS-B2/adquistion-software/ros/openzen/launch_files/copy-launchs-from-catkin_ws-to-repo.bash
+++ sensors/imus-LPMS-B2/adquistion-software/ros/openzen/launch_files/copy-launchs-from-catkin_ws-to-repo.bash
@@ -3,4 +3,7 @@
 cp $HOME/catkin_ws/src/openzenros/launch/lpmsb2-one-display-record.launch .
 cp $HOME/catkin_ws/src/openzenros/launch/lpmsb2-one-playback-data.launch .
 
+cp $HOME/catkin_ws/src/openzenros/launch/lpmsb2-two-display-record.launch .
+cp $HOME/catkin_ws/src/openzenros/launch/lpmsb2-two-playback-data.launch .
+
 
--- sensors/imus-LPMS-B2/adquistion-software/ros/openzen/launch_files/copy-launchs-to-catkin_ws.bash
+++ sensors/imus-LPMS-B2/adquistion-software/ros/openzen/launch_files/copy-launchs-to-catkin_ws.bash
@@ -1,3 +1,7 @@
 
+## One sensor
 cp lpmsb2-one-display-record.launch $HOME/catkin_ws/src/openzenros/launch
 cp lpmsb2-one-playback-data.launch $HOME/catkin_ws/src/openzenros/launch
+
+## Two sensors
+cp lpmsb2-two-display-record.launch $HOME/catkin_ws/src/openzenros/launch
